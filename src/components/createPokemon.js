9cd385fd8adc1059ca7545ca9c552b924b08a92d
Egide Hirwa <66557478+egidehirwa@users.noreply.github.com>
2024-05-21 09:56:54 +0200

    Added styling

--- src/components/createPokemon.js
+++ src/components/createPokemon.js
@@ -6,16 +6,26 @@ const createPokemon = (pokemonData) => {
     container.classList.add('container');
     container.id = 'container';
 
-    // Create title
-    const name = document.createElement('h2');
-    name.classList.add('name');
-    name.id = 'name';
-    name.innerText = pokemonData.name;
+    // Create media wrapper
+    const mediaWrapper = document.createElement('div');
+    mediaWrapper.classList.add('media-wrapper');
 
     // Create Image
     const img = document.createElement('img');
     img.classList.add('image');
     img.src = pokemonData.sprites.front_default;
+
+    mediaWrapper.append(img);
+
+    // Create content wrapper
+    const contentWrapper = document.createElement('div');
+    contentWrapper.classList.add('content-wrapper');
+
+    // Create title
+    const name = document.createElement('h2');
+    name.classList.add('name');
+    name.id = 'name';
+    name.innerText = pokemonData.name;
 
     // Create Abilities
     const abilities = document.createElement('h3');
@@ -25,7 +35,9 @@ const createPokemon = (pokemonData) => {
     // Abilities list
     const abilitiesList = createAbilities(pokemonData.abilities);
 
-    container.append(name, img, abilities, abilitiesList);
+    contentWrapper.append(name, abilities, abilitiesList)
+    
+    container.append(mediaWrapper, contentWrapper);
     return container;
 }
 
